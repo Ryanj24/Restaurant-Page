8f170def5bac1b9650d62c88e610e50943800c17
Ryan Jackson <57965902+Ryanj24@users.noreply.github.com>
2023-03-19 08:55:32 +0000

    Basic homepage styling

--- src/home.js
+++ src/home.js
@@ -1,7 +1,24 @@
 import './styles/index.css'
+import bgImg from './assets/images/main-bg.jpg'
 
 const homeFunction = () => {
     
+    // Get the content container from the HTML page
+    const content = document.getElementById('content');
+    content.style.backgroundImage = `url(${bgImg})`;
+
+    const mainContainer = document.createElement('div');
+    mainContainer.classList.add('main-container');
+
+    const title = document.createElement('h1');
+    title.textContent = 'Lean Cuisine';
+    const slogan = document.createElement('p');
+    slogan.textContent = 'Premium cuisine at an affordable price';
+
+    mainContainer.appendChild(title);
+    mainContainer.appendChild(slogan);
+
+    content.appendChild(mainContainer);
     
 }
 
